dcc5a746bf2df61d3ee030ece3cac261dced098f
Navaneethakrishnan <34976735+smartnava@users.noreply.github.com>
2018-01-16 19:09:14 +0530

    Update p.py

--- p.py
+++ p.py
@@ -19,7 +19,7 @@ s3=list(map(float,input().split()))
 def dd(a):	
 	s1,s2,s3=a
 	for s1,s2,s3 in (list(permutations((s1,s2,s3)))):
-		if all((s1[0]>(s1[1]+s1[2]),s2[1]>(s2[0]+s2[2]),s3[2]>(s3[0]+s3[1]))):return (s1,s2,s3)
+		if all((s1[0]>=(s1[1]+s1[2]),s2[1]>=(s2[0]+s2[2]),s3[2]>=(s3[0]+s3[1]))):return (s1,s2,s3)
 	raise Exception('Not Diagonally Dominant')
 def f2s(a):
 	res=[]
